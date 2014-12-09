9dbac21c515140d72473f27f0282a5652ee3c9e3
Logan Rice <loganrice72@gmail.com>
2014-12-09 12:58:38 -0800

    changed action mailer domain

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   def contact
     AppMailer.contact_me(params[:name], params[:email], params[:message]).deliver
-    flash[:success] = "We look forward to contacting you "
+    flash[:success] = "We look forward to contacting you"
     redirect_to root_path
   end
 end
