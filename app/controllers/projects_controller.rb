300ab6adcc0ad2c3e8b4cbb1cdfcee57680a07f6
Logan Rice <loganrice72@gmail.com>
2014-12-09 12:17:10 -0800

    forgot to remove binding pry

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,5 @@
 class ProjectsController < ApplicationController
   def contact
-    binding.pry
     AppMailer.contact_me(params[:name], params[:email], params[:message]).deliver
     flash[:success] = "We look forward to contacting you "
     redirect_to root_path
